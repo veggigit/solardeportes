440b95bc5fde4b99bfc232715991cba4f63e4c97
sbt000 <46755095+sbt000@users.noreply.github.com>
2019-04-25 23:19:08 -0400

    Update category_default.php
    las sub categorias se dejan en 4 columnas y cel 2 columnas

--- templates/it_theshop2/html/com_jshopping/category/category_default.php
+++ templates/it_theshop2/html/com_jshopping/category/category_default.php
@@ -1,34 +1,86 @@
-
-
 <div class="jshop">
-<h1><?php print $this->category->name?></h1>
-<?php print $this->category->description?>
+    <h1><?php print $this->category->name?></h1>
+    <?php print $this->category->description?>
 
-    
-    <div class="jshop_list_category">
-	<?php if (count($this->categories)){ ?>
+    <div class="container-sub-categorias">
+        <?php if (count($this->categories)){ ?>
 
-        <?php foreach($this->categories as $k=>$category){?>
-            <?php if ($k%$this->count_category_to_row==0) print "<tr>"; ?>
-            <div class = "jshop_categ" style="width:<?php print (100/$this->count_category_to_row)?>%">
-             
-             
-                <div class="image">
-                    <a href = "<?php print $category->category_link;?>"><img class="jshop_img" src="<?php print $this->image_category_path;?>/<?php if ($category->category_image) print $category->category_image; else print $this->noimage;?>" alt="<?php print htmlspecialchars($category->name)?>" title="<?php print htmlspecialchars($category->name)?>" /></a>
+            <?php foreach($this->categories as $k=>$category){?>
+            <div class="col_jshop_categ">
+                <div class="item">
+                    <div class="image">
+                        <a href="<?php print $category->category_link;?>">
+                            <img class="jshop_img"
+                                src="<?php print $this->image_category_path;?>/<?php if ($category->category_image) print $category->category_image; else print $this->noimage;?>"
+                                alt="<?php print htmlspecialchars($category->name)?>"
+                                title="<?php print htmlspecialchars($category->name)?>" />
+                        </a>
+                    </div>
+                    <h3>
+                        <a class="product_link"
+                            href="<?php print $category->category_link?>"><?php print $category->name?>
+                        </a>
+                    </h3>
+                    <p class="category_short_description">
+                        <?php
+                            $puntos = '';
+                            if (strlen($category->short_description) > 48) {
+                                $puntos = '...';
+                            }
+                            print substr($category->short_description, 0, 45).$puntos
+                        ?>
+                    </p>
                 </div>
-              
-                  <h3> <a class = "product_link" href = "<?php print $category->category_link?>"><?php print $category->name?></a></h3>
-                   <p class = "category_short_description"><?php print $category->short_description?></p>
-             
-             </div>
-            
-        <?php } ?>
-       
-    
-    <?php }?>
+            </div>
+            <?php } ?>
+        <?php }?>
     </div>
-    
+
     <?php include(dirname(__FILE__)."/products.php");?>
-    
-    
 </div>
+
+<style>
+    .container-sub-categorias {
+        width: 100%;
+        max-width: 100%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    .container-sub-categorias .col_jshop_categ {
+        width: 25%;
+    }
+
+    .container-sub-categorias .col_jshop_categ .item {
+        padding: 15px 10px;
+        text-align: center;
+        overflow: hidden;
+        height: auto;
+    }
+
+    .container-sub-categorias .col_jshop_categ .item h3 {
+        line-height: 1 !important;
+    }
+
+    .container-sub-categorias .col_jshop_categ .item img {
+        width: 70%;
+        height: 100px;
+        object-fit: cover;
+        object-position: center;
+    }
+
+    @media (max-width: 979px) {
+        .container-sub-categorias .col_jshop_categ {
+            width: 50%;
+        }
+
+        .container-sub-categorias .col_jshop_categ .item {
+            max-height: 15em;
+        }
+
+        .container-sub-categorias .col_jshop_categ .item h3 {
+            font-size: 1.1rem !important;
+        }
+    }
+</style>
